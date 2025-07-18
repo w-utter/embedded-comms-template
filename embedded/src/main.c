4967b176463f93ea015b7ee37c5dd9c7fb4530c4
will <104373134+w-utter@users.noreply.github.com>
2025-07-18 13:21:36 +1000

    nanopb libraries

--- embedded/src/main.c
+++ embedded/src/main.c
@@ -1,6 +1,12 @@
+// pico sdk
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
 
+//protobuf
+#include <pb_encode.h>
+#include <pb_decode.h>
+#include "protos/definitions.pb.h"
 
 int main() {
     stdio_init_all();
